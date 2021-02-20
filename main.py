b85d34dfa4280a5ed972a2e6e3060952dad04fa1
Alexander Rogers <awr90@outlook.com>
2021-02-20 12:20:49 +0000

    get move is used for both players

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from string import ascii_uppercase
+# from string import ascii_uppercase
 from termcolor import colored
 
 # N_COLS = 10
@@ -10,25 +10,25 @@ def clear():
     pass
 
 
-def l():
+def space():
     return ''' '''
 
 
-def printBoard():
-    clear()
-    header = ascii_uppercase[0:N_COLS]
-    print('     ' + '   '.join(header))
-    print('   ╔══' + '═╦══' * (N_COLS - 1) + '═╗')
-    for r in range(0, N_ROWS):
-        entries = [str(l()) for _ in (range(N_COLS))]
-        if r < 10:
-            spacing = ' '
-        else:
-            spacing = ''
-        print(spacing + str(r), '║', ' ║ '.join(entries), '║')
-        if not r == N_ROWS - 1:
-            print('   ╠══' + '═╬══' * (N_COLS - 1) + '═╣')
-    print('   ╚══' + '═╩══' * (N_COLS - 1) + '═╝')
+# def print_board():
+#     clear()
+#     header = ascii_uppercase[0:N_COLS]
+#     print('     ' + '   '.join(header))
+#     print('   ╔══' + '═╦══' * (N_COLS - 1) + '═╗')
+#     for r in range(0, N_ROWS):
+#         entries = [str(space()) for _ in (range(N_COLS))]
+#         if r < 10:
+#             spacing = ' '
+#         else:
+#             spacing = ''
+#         print(spacing + str(r), '║', ' ║ '.join(entries), '║')
+#         if not r == N_ROWS - 1:
+#             print('   ╠══' + '═╬══' * (N_COLS - 1) + '═╣')
+#     print('   ╚══' + '═╩══' * (N_COLS - 1) + '═╝')
 
 
 class Board:
@@ -41,12 +41,15 @@ class Board:
 
     def print(self):
         printable = [([' '] * self.n_cols).copy() for _ in range(self.n_rows)]
+        printable[3] = ['-'] * self.n_cols
         for team in [self.red_team, self.blue_team]:
             for player in team.players:
                 player.print(printable)
+        print('╔═══' + '══' * (self.n_cols - 2) + '══╗')
         for row in printable:
             string = ' '.join(row)
-            print(string)
+            print('║ ' + string + ' ║')
+        print('╚═══' + '══' * (self.n_cols - 2) + '══╝')
 
 
 class Team:
@@ -67,7 +70,14 @@ class Player:
         self.color = color
 
     def move(self, direction):
-        pass
+        if direction == Direction.UP:
+            self.row -= 1
+        elif direction == Direction.DOWN:
+            self.row += 1
+        elif direction == Direction.LEFT:
+            self.column -= 1
+        elif direction == Direction.RIGHT:
+            self.column += 1
 
     def print(self, board):
         # row = list(board[self.row])
@@ -86,12 +96,43 @@ class Gamer:
     pass
 
 
+def get_move(gamer, gamer_1, gamer_2):
+    invalid = False
+    n = ['']
+    c = ['']
+    move_1 = move_2 = -1
+    if gamer == 'gamer_1':
+        move_1 = input(gamer + ' it is your turn \n make a move:')
+    elif gamer == 'gamer_2':
+        move_2 = input(gamer + ' it is your turn \n make a move:')
+    move = move_2 or move_1
+    if not move < 2 or move > 2:
+        invalid = True
+    elif not move[1] == n:
+        invalid = True
+    elif not move[0] == c:
+        invalid = True
+    if invalid:
+        get_move(gamer, gamer_1, gamer_2)
+    else:
+        go(gamer_1, gamer_2, move)
+
+
 def main():
-    # player_1 = input('-> What is your name player 1')
-    # player_2 = input('-> what is your name player 2')
-    # printBoard()
+    gamer_1 = input('-> What is your name player 1')
+    gamer_2 = input('-> what is your name player 2')
     board = Board()
     board.print()
+
+
+def go(gamer_1, gamer_2, move):
+    go_decider = gamer_1
+    if go_decider == gamer_1:
+        go_decider = gamer_2
+    elif go_decider == gamer_2:
+        go_decider = gamer_1
+    else:
+        return go(gamer_1, gamer_2, move)
 
 
 if __name__ == '__main__':
